ddccc02deb6666c572d6d08c52b42cfccea2420a
Emma <e.chee@hotmail.com>
2014-08-18 23:15:48 +1000

    setup jade template engine

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,16 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
+  },
+
+  '/login': {
+    view: 'user/login'
+  },
+
+  '/logout': {
+    view: 'user/logout'
   }
+
 
   /*
   // But what if you want your home page to display
